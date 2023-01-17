fca04343c86cd5ef070bcb66d7d98f9f14871b49
herbq <81512633+stqven@users.noreply.github.com>
2023-01-17 06:38:17 +0200

    wtf

--- sections/product-carousel.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import ProductCarousel from './product-carousel.component'
-
-ReactDOM.render(<ProductCarousel products={initialData} />, document.getElementById("product-carousel"));
